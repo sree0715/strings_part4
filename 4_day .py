a7955c32956adeb741e7943d6cf89bf37b8c468f
sree0715 <125061287+sree0715@users.noreply.github.com>
2023-02-17 22:36:44 +0530

    Add files via upload

---
+++ 4_day .py
@@ -0,0 +1,140 @@
+# a = "welcome"
+# print(a)
+# print(a[1])
+# print(a[0])
+# print(a[2])
+# print(a[3])
+# print("welcome"[6])
+
+# z = '''programming'''
+# print(z[0:1])
+# print(z[2:3])
+# print(z[3:6])
+# print(z[-2:0])
+
+
+# print(type(z))
+# print(dir(str))
+
+# b="pYTHon ProGramme"
+# print(b.capitalize())
+# print(b.islower())
+
+# kite="pINk cLOur"
+# print(kite.casefold())
+# print(kite.center(15))
+# h=kite.encode()
+# print(h)
+# print(type(h))
+# print(h.decode())
+
+# m="helLO woRLD"
+# print(m.title())
+# print(m.istitle())
+# print(m.capitalize())
+# print(m.islower())
+
+# g="white"
+# a=g.encode()
+# print(a)
+# print(type(a))
+# print(a.decode())
+
+# z=' '
+# k='    '
+# print(z.isspace())
+# print(k.isspace())
+
+# N='hEllo pyThon'
+# print(N.startswith(''))
+# print(N.startswith(' '))
+# print(N.endswith('h'))
+# print(len("N"))
+
+# v="bEst fRieNds"
+# print(v.capitalize())
+# print("v".isidentifier())
+# print("3ab".isidentifier())
+# print("_".isidentifier())
+# print("A".isidentifier())
+
+
+# print(v.casefold())
+# print(v.islower())
+# print(v.lower())
+# print(v.center(50,"_"))
+# print(v.center(80))
+
+
+# first="PYtHoN ProGRAMMing laNgUAge"
+# print(first.istitle())
+# print(first.index("n"))
+# print(first.islower())
+# print(first.capitalize())
+# print(first.strip())
+
+# date="  Time Year        "
+# print(date.rstrip())
+# print(date.lstrip())
+# print(date.strip())
+
+# print(first.rindex("N"))
+# print(first.index("g"))
+
+# y={}
+# print(type(y))
+
+
+# k="python programme"
+# print(k.count("m"))
+# print(k.count("o")) 
+# print(k.replace('programme','language'))
+# print(len(k))
+
+# e="first","second","third"
+# d='*'.join(e)
+# print(d)
+
+
+# a="5helHO"
+# print(a.isalnum())
+# print(a.swapcase())
+# print(a.isalpha())
+
+# s="tiger is animal"
+# print(s.split(" "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
